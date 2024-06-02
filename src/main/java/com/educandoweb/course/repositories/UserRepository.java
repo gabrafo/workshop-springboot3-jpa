d3c322d62db28650b1e14f59f6fa574055ce2c02
Gabriel Fagundes <gafagundesmesquita@gmail.com>
2024-06-02 19:41:54 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course/repositories/UserRepository.java
+++ src/main/java/com/educandoweb/course/repositories/UserRepository.java
@@ -2,8 +2,10 @@ package com.educandoweb.course.repositories;
 
 import com.educandoweb.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 // Interface que utiliza métodos do JPARepository para facilitar operações nos dados
 // Já vem com implementação padrão, então não é preciso implementarmos!
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }

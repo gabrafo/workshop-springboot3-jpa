8428e2f54a94dfa9b39938fab98cb0c78acbb08c
Gabriel Fagundes <gafagundesmesquita@gmail.com>
2024-06-03 10:01:17 -0300

    Product

---
+++ src/main/java/com/educandoweb/course/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.educandoweb.course.repositories;
+
+import com.educandoweb.course.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+// Interface que utiliza métodos do JPARepository para facilitar operações nos dados
+// Já vem com implementação padrão, então não é preciso implementarmos!
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
